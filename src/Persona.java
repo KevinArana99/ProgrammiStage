5348148fd34d9e11765db546fd68b973eb86ac3d
Kevin <Kevin@DESKTOP-0BE6831>
2018-09-11 15:32:29 +0200

    Modifica programma

--- src/Persona.java
+++ src/Persona.java
@@ -5,17 +5,9 @@
  */
 /**
  *
- * @author vubi
+ * @author arana
  */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Vubi
- */
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
